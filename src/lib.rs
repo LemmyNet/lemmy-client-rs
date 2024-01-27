f8cd71601eee203b1a6b583b985570873773f584
SleeplessOne1917 <insomnia-void@protonmail.com>
2024-01-27 15:13:37 -0500

    Make private "god" trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use serde::{Deserialize, Serialize};
+use http::method::Method;
+
+pub struct LemmyRequest<R: Serialize> {
+  pub body: Option<R>,
+  pub jwt: Option<String>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<R: Serialize> LemmyRequest<R> {
+  pub fn from_jwt(jwt: Option<String>) -> Self {
+    Self {
+      body: None::<R>,
+      jwt,
     }
+  }
+}
+
+impl<R: Serialize> From<R> for LemmyRequest<R> {
+  fn from(body: R) -> Self {
+    LemmyRequest {
+      body: Some(body),
+      jwt: None,
+    }
+  }
+}
+
+mod private_trait {
+  use async_trait::async_trait;
+use super::{Deserialize, Serialize, LemmyRequest, Method};
+
+
+  #[async_trait(?Send)]
+  pub trait LemmyClient {
+    async fn make_request<Response, Form, Request>(
+      &self,
+      method: Method,
+      path: &str,
+      form: Request,
+    ) -> Result<Response, ()>
+    where
+      Response: for<'de> Deserialize<'de>,
+      Form: Serialize,
+      Request: Into<LemmyRequest<Form>>;
+  }
 }
