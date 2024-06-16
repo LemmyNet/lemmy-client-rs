e8456179155c27eb2d0ce9871c01b4dd74f526d4
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-06-16 13:44:19 -0400

    Fix bug in docs

--- src/lib.rs
+++ src/lib.rs
@@ -22,9 +22,10 @@
 //!   assert!(res.is_ok());
 //! }
 //!
-//! ## IMPORTANT NOTICE
-//! This crate now uses a different versioning scheme than before so as not to be too tied down to Lemmy releases. For Lemmy versions 0.19.4 and up, use versions 1.x.x. For Lemmy versions 0.19.3 and under, use versions 0.19.5 and up. This is confusing, but should become a non issue as Lemmy accumulates versions and fewer servers use Lemmy versions use 0.19.3 and lower.
 //! ```
+//! //! ## IMPORTANT NOTICE
+//! This crate now uses a different versioning scheme than before so as not to be too tied down to Lemmy releases. For Lemmy versions 0.19.4 and up, use versions 1.x.x. For Lemmy versions 0.19.3 and under, use versions 0.19.5 and up. This is confusing, but should become a non issue as Lemmy accumulates versions and fewer servers use Lemmy versions use 0.19.3 and lower.
+
 use std::collections::HashMap;
 
 use crate::{lemmy_client_trait::LemmyClientInternal, response::LemmyResult};
