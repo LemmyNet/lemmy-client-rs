9e7481fe9f82d264e1450fab3f4d7e9983d35136
Dessalines <tyhou13@gmx.com>
2026-05-29 22:26:17 -0400

    Adding report

--- src/endpoints/report.rs
+++ src/endpoints/report.rs
@@ -1,41 +1,18 @@
 use crate::{LemmyClient, LemmyResult};
 use http::Method;
-use lemmy_api_common::report::{
-  CommunityReportResponse,
-  CreateCommunityReport,
-  ListReports,
-  ListReportsResponse,
+use lemmy_api_common::{
+  PagedResponse,
+  report::{ListReports, ReportCombinedView},
 };
 
 impl LemmyClient {
   /// List all reports.
   ///
   /// HTTP GET /report/list
-  pub async fn list_reports(&self, data: ListReports) -> LemmyResult<ListReportsResponse> {
+  pub async fn list_reports(
+    &self,
+    data: ListReports,
+  ) -> LemmyResult<PagedResponse<ReportCombinedView>> {
     self.make_request(Method::GET, "report/list", data).await
-  }
-
-  /// Report a community.
-  ///
-  /// HTTP POST /community/report
-  pub async fn report_community(
-    &self,
-    data: CreateCommunityReport,
-  ) -> LemmyResult<CommunityReportResponse> {
-    self
-      .make_request(Method::POST, "community/report", data)
-      .await
-  }
-
-  /// Resolves a community report.
-  ///
-  /// HTTP PUT /community/report/resolve
-  pub async fn resolve_community_report(
-    &self,
-    data: CreateCommunityReport,
-  ) -> LemmyResult<CommunityReportResponse> {
-    self
-      .make_request(Method::PUT, "community/report/resolve", data)
-      .await
   }
 }
