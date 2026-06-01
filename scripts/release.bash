cc73474282b44e50078ddf4d894b2ac36808bbbc
Dessalines <dessalines@users.noreply.github.com>
2026-06-01 14:59:24 -0400

    Release 019 script (#79)
    * Revert "fix(deps): update rust crate reqwest to 0.13 (#55)"
    
    This reverts commit 51ea8d7e11f65ec1ed3afcca8073e57b777e3acb.
    
    * Adding 0.19 release script
    
    * Fixing gitignore
    
    * Version 0.19.18-test-2
    
    * Fixing woodpecker
    
    * Fixing fmt

---
+++ scripts/release.bash
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# Creating the new tag
+new_tag="$1"
+
+# Goto the upper route
+CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+cd "$CWD/../"
+
+# Update crate versions
+old_tag=$(grep version Cargo.toml | head -1 | cut -d'"' -f 2)
+sed -i "s/{ version = \"=$old_tag\", path/{ version = \"=$new_tag\", path/g" Cargo.toml
+sed -i "s/version = \"$old_tag\"/version = \"$new_tag\"/g" Cargo.toml
+
+# Run check to ensure translations are valid and lockfile is updated
+cargo check
+
+# The commit
+git add Cargo.toml Cargo.lock
+git commit -m"Version $new_tag"
+git tag $new_tag
+
+# Push
+git push origin $new_tag
+git push
